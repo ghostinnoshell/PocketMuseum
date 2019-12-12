94a3b6761996cb6949dfbf8c7edd48bc4242234a
HaochenQ <haochenqi1997@gmail.com>
2019-12-12 13:40:49 -0500

    blog

--- js/blogjs.js
+++ js/blogjs.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
     load();
 });
 
@@ -7,14 +7,15 @@ async function post(e) {
     let postContent = document.getElementById("postContent").value;
     let userName = localStorage.getItem('current');
     let jwt = localStorage.getItem(`${userName}`);
-
+    // alert(jwt);
     let root = e.target;
-    alert($(root).siblings('table'));
+    // alert($(root).siblings('table'));
 
     let content = {
         "content": postContent,
     };
     async function postBlog() {
+
         const response = await axios({
             method: 'post',
             url: `http://localhost:3000/private/${userName}`,
@@ -26,19 +27,47 @@ async function post(e) {
         return response;
     }
 
-    postBlog();
+    function loadPosts() {
+        let box = $('.box');
+        let $postCard = $('<div></div>')
+            // alert("post");
+        async function getBlog() {
+            const response = await axios({
+                method: 'get',
+                url: `http://localhost:3000/private/`,
+                headers: { Authorization: `${jwt}` },
+            });
+            return response;
+        }
+        getBlog().then(function(allPost) {
+            allPost.forEach(element => {
+
+            });
+        });
+
+
+    }
+    postBlog().then(
+        function(e) {
+            loadPosts();
+        }
+    );
+
+
 
     //     $(root).replaceWith(
     //         `<div id = "blog"> </div>`
     //     );
     //     $('#blog').append(`<div> ${postContent} </div>`);
     // }
+
 }
+
 
 
 function load() {
     const $root = $('.box');
-    $root.on("click", '#post', function (e) {
+    $root.on("click", '#post', function(e) {
         e.preventDefault();
         post(e);
     });
